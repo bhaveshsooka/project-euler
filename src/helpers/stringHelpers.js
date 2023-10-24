30e01b4786d47d1a0bc19ab28334737150c990f6
Bhavesh Sooka <bsooka@gmail.com>
2023-10-24 08:58:11 +0200

    refactor re-usable method

--- src/helpers/stringHelpers.js
+++ src/helpers/stringHelpers.js
@@ -9,7 +9,17 @@ function isPalindrome(string) {
   return string.split("").reverse().join("") == string;
 }
 
+function sumDigits(num) {
+  let stringNum = num.toString();
+  let sum = 0;
+  for (let i = 0; i < stringNum.length; i++) {
+    sum += Number(stringNum[i]);
+  }
+  return sum;
+}
+
 module.exports = {
   arraysEqual,
   isPalindrome,
+  sumDigits,
 };
--- src/problems/problem0016.js
+++ src/problems/problem0016.js
@@ -1,18 +1,11 @@
 "use strict";
 
+const stringHelpers = require("../helpers/stringHelpers");
+
 function solve(base, exponent) {
   let power = BigInt(base ** exponent).toString();
 
-  return sumDigits(power);
-}
-
-function sumDigits(num) {
-  let stringNum = num.toString();
-  let sum = 0;
-  for (let i = 0; i < stringNum.length; i++) {
-    sum += Number(stringNum[i]);
-  }
-  return sum;
+  return stringHelpers.sumDigits(power);
 }
 
 module.exports = {
